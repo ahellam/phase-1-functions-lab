eca479620141c6d4fd92fe07baebdb5e9659387e
Aaron Hellam <aaronhellam@gmail.com>
2022-02-23 10:32:51 -0800

    commit tree 833c96bd0b2baa8b433876f775ec6abcd2dd9d57

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    let scuberHQ = 42;
+    return Math.abs(blocks - scuberHQ)
+}
+function distanceFromHqInFeet(feet) {
+   return (distanceFromHqInBlocks(feet) * 264);
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs((start-destination)*264)
+}
+
+function calculatesFarePrice(start, destination) {
+    let lowPrice = Math.abs((distanceTravelledInFeet(start, destination) -400 ) * .02)
+    let highPrice = 25
+
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0;
+    } else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return 'cannot travel that far';
+    } else if (distanceTravelledInFeet(start, destination) > 2000) {
+        return highPrice;
+    } else if (distanceTravelledInFeet(start, destination) >= 400) {
+        return lowPrice;
+    } 
+}
+console.log(calculatesFarePrice(50, 58));
